0adb0e68f35e6b628f19434af5345f2e68db784d
Josiah VanderZee <josiah_vanderzee@mediacombb.net>
2023-02-13 05:05:42 -0600

    Pass member map by const ref
    Signed-off-by: Josiah VanderZee <josiah_vanderzee@mediacombb.net>

--- src/expense_balancer.h
+++ src/expense_balancer.h
@@ -5,6 +5,7 @@
 #include <ostream>
 #include <string>
 
-void print_balances(std::map<std::string, double> members, std::ostream& os);
+void print_balances(const std::map<std::string, double>& members,
+                    std::ostream& os);
 
 #endif // EXPENSE_BLANCER_H_INCLUDED
